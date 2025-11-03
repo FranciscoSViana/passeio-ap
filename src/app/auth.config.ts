09689d4f6b98d07385f5b4313d08ac6c8daf785f
Francisco Viana <franciscosantos29@gmail.com>
2025-11-03 14:22:57 -0300

    Corrigindo para fazer o gh pages

--- src/app/auth.config.ts
+++ src/app/auth.config.ts
@@ -2,7 +2,7 @@ import { AuthConfig } from 'angular-oauth2-oidc';
 
 export const auth: AuthConfig = {
     issuer: 'https://accounts.google.com',
-    redirectUri: window.location.origin,
+    redirectUri: window.location.origin + '/passeio-app',
     clientId: '1021068405977-otdmqrods37b0jcj8peprrdgem383i0o.apps.googleusercontent.com',
     scope: 'openid profile email',
     strictDiscoveryDocumentValidation: false
